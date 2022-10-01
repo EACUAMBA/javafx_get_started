025dac2b0aa4d35d64c548f73ff9265212a072d1
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-10-01 22:17:09 +0200

    Criei o webservice utilizando spring reactive que retorna a cada segundo um valor do preço do stock no mercado utilizando servers stream event SSE.

---
+++ stock_client/src/main/java/com/eacuamba/reactive_spring_boot_and_javafx/stock_client/StockClientApplication.java
@@ -0,0 +1,13 @@
+package com.eacuamba.reactive_spring_boot_and_javafx.stock_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockClientApplication.class, args);
+    }
+
+}
---
+++ stock_service/src/test/kotlin/com/eauamba/reactive_spring_boot_and_javafx/demo/stock_service/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eauamba.reactive_spring_boot_and_javafx.demo.stock_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
